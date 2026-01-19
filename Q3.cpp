1bd9d467cf4bf8c9568487fd015851f4c6cc79d4
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-19 10:03:01 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First Number";// << -> Insertion Operator
+    cin>>a;                     // >> -> Extraction Operator
+    cout<<"Enter Second Number";
+    cin>>b;
+    cout<<"The sum is "<<a+b;
+    return 0;
+}
