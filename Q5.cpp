368be289fba4df4ada13e8b51464123539f2bcd7
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-19 10:14:46 +0530

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter First Number";
+    cin>>a;                    
+    cout<<"Enter Second Number";
+    cin>>b;
+    cout<<"The product is "<<a*b;
+    return 0;
+}
---
+++ Q6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Enter First Number";
+    cin>>a;                    
+    cout<<"Enter Second Number";
+    cin>>b;
+    cout<<"The quotient is "<<a/b;
+    return 0;
+}
