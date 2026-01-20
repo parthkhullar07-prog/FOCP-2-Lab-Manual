3abde77446e9fa7c61605cc8b1b0454d8d235683
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-20 13:15:18 +0530

    Include bonus calculation in salary output
    Added output for bonus calculation before displaying the net salary.

--- Q6.cpp
+++ Q6.cpp
@@ -8,6 +8,7 @@ int main(){
     cout<<"Enter Basic Salary";
     cin>>b;
     c=b+b*(12.0/100.0);
+    cout<<"Bonus "<<b*(12.0/100.0)<<endl;
     cout<<"Basic Salary After Bonus "<<c<<endl;
     cout<<"Net Salary "<<c*a;
     return 0;
