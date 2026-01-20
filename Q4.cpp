4f33f79ef094f6e7bf5ce92c1cd2fa635281ab00
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-20 13:05:01 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    cout<<"Enter First Number";
-    cin>>a;                    
-    cout<<"Enter Second Number";
+    int a,b,c;
+    cout<<"Enter Item No.";
+    cin>>a;
+    cout<<"Enter Quantity";
     cin>>b;
-    cout<<"The difference is "<<a-b;
+    cout<<"Enter Unit Price";
+    cin>>c;
+    cout<<"The Amount Before Discount is "<<b*c<<endl;
+    float aftdisc;
+    aftdisc=(80.0/100.0)*b*c;
+    cout<<"The Amount After Discount is "<<aftdisc;
     return 0;
 }
