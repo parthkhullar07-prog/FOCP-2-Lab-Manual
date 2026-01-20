6f291865175ee76c79217391b059393dcf2aa86b
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-20 12:41:50 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";
+    int a,b,c;
+    cout<<"Enter First Marks";
+    cin>>a;
+    cout<<"Enter Second Marks";
+    cin>>b;
+    cout<<"Enter Third Marks";
+    cin>>c;
+    cout<<"The average is "<<(a+b+c)/3;
     return 0;
 }
