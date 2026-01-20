1555b42d78724894cdf083aa2f16ac06a421e3e3
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-20 12:45:43 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    a=5;
-    b=9;
-    cout<<a+b;
+    int a;
+    float pi =3.14;
+    cout<<"Enter Radius Of The Circular Fountain ";
+    cin>>a;
+    cout<<"The Area Covered By the Fountain Is "<<pi*a*a<<" Units";
     return 0;
 }
