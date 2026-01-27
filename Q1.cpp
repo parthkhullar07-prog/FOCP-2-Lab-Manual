01704960dc2aa60e2e92d9884008cd5eb7aa95ed
parthkhullar07-prog <parthkhullar07@gmail.com>
2026-01-27 21:53:43 +0530

    Refactor average calculation into Avg class
    Refactored the program to use a class for calculating the average of three marks instead of a procedural approach.

--- Q1.cpp
+++ Q1.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
 using namespace std;
+class Avg{
+    private:
+    int marks1 , marks2 , marks3;
+    float average;
+    public:
+    void getData();
+    void avg();
+};
+void Avg::getData(){
+    cout<<"Enter marks1 ";
+    cin>>marks1;
+    cout<<"Enter marks2 ";
+    cin>>marks2;
+    cout<<"Enter marks3 ";
+    cin>>marks3;
+}
+void Avg ::avg(){
+    cout<<"Average of these Marks is ";
+    average=(marks1+marks2+marks3)/3;
+    cout<<average;
+}
 int main(){
-    int a,b,c;
-    cout<<"Enter First Marks";
-    cin>>a;
-    cout<<"Enter Second Marks";
-    cin>>b;
-    cout<<"Enter Third Marks";
-    cin>>c;
-    cout<<"The average is "<<(a+b+c)/3;
+    Avg a1;
+    a1.getData();
+    a1.avg();
     return 0;
 }
